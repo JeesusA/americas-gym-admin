19bdf1c659f3954a96ce3e5524207f28c9ce4e05
jesus <Al07017485@tecmilenio.mx>
2025-01-23 19:14:12 -0600

    Remplace el panel de edicion por un menu de navegacion con un icono de hamburguesa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Section from "./components/Section";
-import "./App.css"
+import "./App.css";
 
 const App = () => {
 
@@ -11,49 +11,52 @@ const App = () => {
   // Función para cambiar la sección activa
   const handleSectionChange = (sectionId) => {
     setActiveSection(sectionId);
-  }
+  };
 
   return (
     <div className="container">
-      <Sidebar onSectionSelect={handleSectionChange}/>
-      <main className="content">
-        {/* Mostrar solo la sección activa */}
-        {activeSection === "precios" && (<Section
-          id="precios"
-          title="Editar Precios de Membresías"
-          description="Aquí podrás agregar, editar o eliminar los precios de las membresías."
-          buttonText="Agregar nuevo precio"
-        />)}
-
-        {activeSection === "promociones" && (<Section
-          id="promociones"
-          title="Editar Promociones"
-          description="Aquí podrás agregar, editar o eliminar las promociones disponibles."
-          buttonText="Agregar nueva promoción"
-        />)}
-        
-        {activeSection === "informacion-general" && (<Section
-          id="informacion-general"
-          title="Editar Información General"
-          description="Aquí podrás actualizar la información básica del gimnasio."
-          buttonText="Editar información"
-        />)}
-        
-        {activeSection === "sucursales" && (<Section
-          id="sucursales"
-          title="Editar Información de Sucursales"
-          description="Aquí podrás agregar, editar o eliminar sucursales."
-          buttonText="Agregar nueva sucursal"
-        />)}
-
-        {activeSection === "contacto" && (<Section
-          id="contacto"
-          title="Editar Información de Contacto"
-          description="Aquí podrás actualizar el correo, teléfono y redes sociales."
-          buttonText="Editar contacto"
-        />)}
-        
-      </main>
+      <Sidebar onSectionSelect={handleSectionChange} />
+      {/* Mostrar solo la sección activa */}
+      {activeSection === "precios" && (
+          <Section
+            id="precios"
+            title="Editar Precios de Membresías"
+            description="Aquí podrás agregar, editar o eliminar los precios de las membresías."
+            buttonText="Agregar nuevo precio"
+          />
+        )}
+        {activeSection === "promociones" && (
+          <Section
+            id="promociones"
+            title="Editar Promociones"
+            description="Aquí podrás agregar, editar o eliminar las promociones disponibles."
+            buttonText="Agregar nueva promoción"
+          />
+        )}
+        {activeSection === "informacion-general" && (
+          <Section
+            id="informacion-general"
+            title="Editar Información General"
+            description="Aquí podrás actualizar la información básica del gimnasio."
+            buttonText="Editar información"
+          />
+        )}
+        {activeSection === "sucursales" && (
+          <Section
+            id="sucursales"
+            title="Editar Información de Sucursales"
+            description="Aquí podrás agregar, editar o eliminar sucursales."
+            buttonText="Agregar nueva sucursal"
+          />
+        )}
+        {activeSection === "contacto" && (
+          <Section
+            id="contacto"
+            title="Editar Información de Contacto"
+            description="Aquí podrás actualizar el correo, teléfono y redes sociales."
+            buttonText="Editar contacto"
+          />
+        )}
     </div>
   );
 };
